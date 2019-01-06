e8ff8a86237040ff33a5927d51cd6458122d0942
Jonathan Rodriguez <jarodriguez3719@misena.edu.co>
2019-01-06 17:48:22 -0500

    añadiendo condicion del evento del menu

--- js/app.js
+++ js/app.js
@@ -1,15 +1,21 @@
 // document.querySelector('.header nav').style.transform = "translateX(0%)";
 
+const response = window.matchMedia('screen and (max-width: 768px)');
 const menu = document.querySelector('.menu');
-
 const boton = document.querySelector('.boton');
 
 
-boton.addEventListener('click', hideShow);
+response.addListener(validation);
 
+function validation(event) {
+  if (event.matches) {
+    boton.addEventListener('click', hideShow);
+  } else {
+    boton.removeEventListener('click', hideShow);
+  }
+}
 
 function hideShow() {
-   console.log('hola');
    if(menu.classList.contains('menuactive')) {
     menu.classList.remove('menuactive');
    } else {
